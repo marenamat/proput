ae05ebdae68a071c344854e633448029bde968d6
Maria Matejka <mq@ucw.cz>
2022-01-19 18:18:24 +0100

    First try on poll and read method implementation for the chardev

--- kernel/proput.c
+++ kernel/proput.c
@@ -14,14 +14,105 @@
 
 #include "proput.h"
 
-static _Bool ufp_connected = 0;
-
 struct proput_bitstream {
   unsigned short len;
   unsigned short pos;
   unsigned int repeat;
   unsigned char data[0];
 };
+
+#define READ_BUF_SZ	256
+struct ufp_context {
+  struct ufp_context *prev, *next;
+  struct poll_table_struct *poll_read_pending;
+  wait_queue_head_t read_queue;
+  struct semaphore sem;
+  u16 read_end;
+  char read_buf[READ_BUF_SZ];
+};
+
+DEFINE_MUTEX(ufp_mutex);
+
+static __poll_t ufp_poll(struct file *fp, struct poll_table_struct *pts)
+{
+  struct ufp_context *ctx = fp->private_data;
+
+  if (down_interruptible(&ctx->sem))
+    return -ERESTARTSYS;
+
+  if (ctx->read_end > 0)
+  {
+    up(&ctx->sem);
+    return POLLWR | POLLRD;
+  }
+  
+  else if (ctx->poll_read_pending)
+  {
+    up(&ctx->sem);
+    return -EBUSY;
+  }
+
+  ctx->poll_read_pending = pts;
+  up(&ctx->sem);
+  return POLLWR;
+}
+
+static ssize_t ufp_read(struct file *fp, char __user *buf, size_t sz, loff_t *off)
+{
+  struct ufp_context *ctx = fp->private_data;
+
+  if (sz < sizeof(struct proput_response_header))
+    return -EINVAL;
+
+  if (down_interruptible(&ctx->sem))
+    return -ERESTARTSYS;
+
+  if (ctx->read_end == 0)
+  {
+    if (fp->f_flags & O_NONBLOCK)
+      return -EAGAIN;
+
+    up(&ctx->sem);
+
+    if (wait_event_interruptible(ctx->read_queue, (ctx->read_end > 0)))
+      return -ERESTARTSYS;
+
+    if (down_interruptible(&ctx->sem))
+      return -ERESTARTSYS;
+  }
+
+  char *response;
+  uint16_t len;
+
+  if (ctx->read_end > sz)
+  {
+    struct proput_response_header prh = {
+      .response = PROPUT_R_BUF,
+      .len = ctx->read_end,
+    };
+
+    response = &prh;
+    len = sizeof(struct proput_response_header);
+  }
+  else
+  {
+    response = ctx->read_buf;
+    len = ctx->read_end;
+    ctx->read_end = 0;
+  }
+
+  if (copy_to_user(buf, response, len)) {
+    up(&ctx->sem);
+    return -EFAULT;
+  }
+
+  up(&ctx->sem);
+  return len;
+}
+
+static ssize_t ufp_write(struct file *fp, const char __user *buf, size_t sz, loff_t *off)
+{
+}
 
 static int ufp_open(struct inode *inode, struct file *file)
 {
@@ -44,8 +135,8 @@ static int ufp_release(struct inode *inode, struct file *file)
 }
 
 static struct file_operations ufp = {
-//  .read = ufp_read,
-//  .write = ufp_write,
+  .read = ufp_read,
+  .write = ufp_write,
   .open = ufp_open,
   .release = ufp_release,
 };
@@ -54,7 +145,6 @@ static int ufp_major;
 
 int __init proput_init(void)
 {
-  printk(KERN_INFO "ProPuT inserted\n");
   ufp_major = register_chrdev(0, "proput", &ufp);
   if (ufp_major < 0)
   {
@@ -62,6 +152,7 @@ int __init proput_init(void)
     return ufp_major;
   }
 
+  printk(KERN_INFO "ProPuT inserted\n");
   return 0;
 }
 
--- kernel/proput.h
+++ kernel/proput.h
@@ -11,11 +11,23 @@
 
 #define PROPUT_TYPE_SEND_BITS	  1
 
-struct proput_send_bits {
-  unsigned short type;
-  unsigned short len;
-  unsigned int repeat;
+#define PROPUT_CMD_VAL(s)   ((s[0] << 8) + s[1])
+
+struct proput_cmd_header {
+  uint16_t cmd;
+  uint16_t len;
   unsigned char data[0];
 };
+
+struct proput_response_header {
+  uint16_t response;
+  uint16_t len;
+  unsigned char data[0];
+};
+
+#define PROPUT_C_DEVLIST	PROPUT_CMD_VAL("DL")
+
+#define PROPUT_R_DEVLIST	PROPUT_CMD_VAL("DL")
+#define PROPUT_R_BUF		PROPUT_CMD_VAL("Bu")
 
 #endif
